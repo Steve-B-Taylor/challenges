bb16e49406e597bd50abdc3d7b75f58b1ca9d64d
Steve Taylor <malakesh@hotmail.com>
2021-03-02 09:01:12 -0800

    update popup

--- Week7/pop-up-ad/src/components/Popup.js
+++ Week7/pop-up-ad/src/components/Popup.js
@@ -2,18 +2,36 @@ import React from "react";
 import PerformanceList from "./PerformanceList";
 
 const Popup = (props) => {
+  const handleExit = () => {
+    confirm("Are you sure you don't want to see Tay Swift???");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Congrats on your tix!");
+  };
   return (
     <div className="popup-parent">
       <div id="popup-container" className="popup-child-1">
-        <i id="close-x" className="fas fa-times"></i>
+        <i id="close-x" className="fas fa-times" onClick={handleExit}></i>
         <h2 id="title">You have won tickets to see Taylor Swift!!!</h2>
         <h4 id="body">
           Please enter your email so we can send you the tickets
         </h4>
         <form id="email-form">
           <label htmlFor="email">
-            <input type="text" id="email" name="email"></input>
-            <input type="submit" id="submit" value="CLAIM YOUR PRIZE!"></input>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email here"
+            ></input>
+            <input
+              type="submit"
+              id="submit"
+              value="CLAIM YOUR PRIZE!"
+              onClick={handleSubmit}
+            ></input>
           </label>
         </form>
         <PerformanceList />
--- Week7/pop-up-ad/src/main.js
+++ Week7/pop-up-ad/src/main.js
@@ -1,25 +1,6 @@
 import "./app.scss";
-
-// import depencies like React and ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
-
-// import components, if rendered in ReactDOM.render
 import Popup from "./components/Popup";
 
-// call on ReactDOM.render with the correct arguments
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="popup-parent">
-//         <div className="popup-child-1">
-//           <Popup />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<App />, document.getElementById("app"));
 ReactDOM.render(<Popup />, document.getElementById("app"));
