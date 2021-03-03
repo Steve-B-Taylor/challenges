c53014171ef0c76a9c30e1b1cf3ca0aaec90bb13
Steve Taylor <malakesh@hotmail.com>
2021-03-03 07:33:26 -0800

    complete twitter, add stateful

--- Week7/twitter-feed/src/components/Tweet.js
+++ Week7/twitter-feed/src/components/Tweet.js
@@ -1,2 +1,91 @@
-const Tweet = () => null;
+import React from "react";
+
+const Tweet = (props) => {
+  const entityData = props.entities;
+  const userData = props.user;
+
+  let ts = parseInt(props.timestamp);
+  let [day, month, date] = new Date(ts).toDateString("en-US").split(" ");
+  // let date = new Date(ts);
+  console.log(`${month} ${date}`);
+
+  let mediaUrl = "";
+  let retweet = "fas fa-retweet tweetMeta-gap";
+  let liked = "fas fa-heart tweetMeta-gap";
+
+  if (entityData) {
+    mediaUrl = <img src={entityData[0].media_url} />;
+  }
+
+  if (props.retweeted) {
+    retweet += " retweet";
+  }
+
+  if (props.liked) {
+    liked += " liked";
+  }
+
+  const clickReply = () => {
+    window.alert("Reply");
+  };
+
+  const clickRetweet = () => {
+    window.alert("Retweet");
+  };
+
+  const clickLiked = () => {
+    window.alert("Liked");
+  };
+
+  const clickMore = () => {
+    window.alert("More");
+  };
+
+  return (
+    <div className="tweetContainer">
+      <div className="tweet-upper-container">
+        <div className="user-image-url">
+          <img src={userData.profile_image_url} />
+        </div>
+        <div className="userInfo">
+          <ul>
+            <li>
+              <span className="user-name">{userData.name}</span>
+              <span className="user-screen-name"> @{userData.screen_name}</span>
+              <span className="user-post-date">
+                {" "}
+                {month} {date}
+              </span>
+            </li>
+            <li>{props.text}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="tweetMedia">{mediaUrl}</div>
+      <div className="tweet-lower-container">
+        <div className="tweetMeta">
+          <span>
+            <i className="fas fa-reply tweetMeta-gap" onClick={clickReply}></i>
+          </span>
+          <span>
+            <i className={retweet} onClick={clickRetweet}>
+              <span className="tweetMeta-gap-right">{props.retweetCount}</span>
+            </i>
+          </span>
+          <span>
+            <i className={liked} onClick={clickLiked}>
+              <span className="tweetMeta-gap-right">{props.favoriteCount}</span>
+            </i>
+          </span>
+          <span>
+            <i
+              className="fas fa-ellipsis-h tweetMeta-gap"
+              onClick={clickMore}
+            ></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Tweet;
--- Week7/twitter-feed/src/components/TwitterFeed.js
+++ Week7/twitter-feed/src/components/TwitterFeed.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import data from "../constants/data";
+import Tweet from "./Tweet";
 
 const TwitterFeed = (props) => {
-  return(
-    <div />
-  )
+  const dataMap = data.map((element) => {
+    return (
+      <Tweet
+        key={element.id_str}
+        text={element.text}
+        user={element.user}
+        entities={element.entities.media}
+        liked={element.liked}
+        favoriteCount={element.favorite_count}
+        retweetCount={element.retweet_count}
+        retweeted={element.retweeted}
+        timestamp={element.timestamp_ms}
+      />
+    );
+  });
+  return <div>{dataMap}</div>;
 };
 
 export default TwitterFeed;
