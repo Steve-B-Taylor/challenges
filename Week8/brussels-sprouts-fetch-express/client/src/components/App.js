964b92222f288620ad8628d4e8808da2833a0947
Steve Taylor <malakesh@hotmail.com>
2021-03-09 09:57:20 -0800

    Update recipes

--- Week8/brussels-sprouts-fetch-express/client/src/components/App.js
+++ Week8/brussels-sprouts-fetch-express/client/src/components/App.js
@@ -1,43 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import { hot } from "react-hot-loader/root"
 
-import RandomSprout from './RandomSprout'
-import SproutsIndex from './SproutsIndex'
+import RandomSprout from "./RandomSprout"
+import SproutsIndex from "./SproutsIndex"
 
 const App = (props) => {
-  const [recipe, setRecipe] = useState("")
+  const [randomRecipe, setRandomRecipe] = useState("")
   const [recipes, setRecipes] = useState([])
 
-  const getRandomRecipe = () => {
-    // YOUR FETCH CALL HERE
+  const getRandomRecipe = async () => {
+    try {
+      const response = await fetch("/api/v1/recipes/random")
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw error
+      }
+      const recipeData = await response.json()
+      setRandomRecipe(recipeData.recipe)
+      // console.log(`Body: ${recipeData.recipe}`)
+    } catch (err) {
+      console.error(`Error in fetch: ${err.message}`)
+    }
   }
 
-  const getAllRecipes = () => {
-    // YOUR FETCH CALL HERE
+  const getAllRecipes = async () => {
+    try {
+      const response = await fetch("/api/v1/recipes")
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw error
+      }
+      const recipeData = await response.json()
+      setRecipes(recipeData.recipes)
+      // console.log(`Body: ${recipeData.recipe}`)
+    } catch (err) {
+      console.error(`Error in fetch: ${err.message}`)
+    }
   }
 
   const handleRandomClick = () => {
     getRandomRecipe()
+    setRecipes([])
   }
 
   const handleIndexClick = () => {
     getAllRecipes()
+    setRandomRecipe("")
   }
 
-  return(
+  return (
     <div className="container">
       <h1>Sprout Fetcher</h1>
-      <RandomSprout
-        recipe={recipe}
-      />
-      <SproutsIndex
-        recipes={recipes}
-      />
 
       <div className="buttons">
-        <button onClick={handleRandomClick} className="btn">Get Random Recipe</button>
+        <button onClick={handleRandomClick} className="btn">
+          Get Random Recipe
+        </button>
 
-        <button onClick={handleIndexClick} className="btn">See All Recipes</button>
+        <button onClick={handleIndexClick} className="btn">
+          See All Recipes
+        </button>
+
+        <RandomSprout recipe={randomRecipe} />
+        <SproutsIndex recipes={recipes} />
       </div>
     </div>
   )
